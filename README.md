a9bc38b7ab09096c9aca5a4ea715a63e25249337
Owain Kenway <o.kenway@ucl.ac.uk>
2016-02-10 16:26:02 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # licenselogparse
 Tools for parsing FlexLM logs
+
+Currently there is a single tool in this repository, maxlic.py.
+
+##maxlic.py
+
+Maxlic.py analyses a FlexLM license manager log file and then prints out the maximum usage of each product it detects, e.g.:
+
+```none
+$ ./maxlic.py -l logs/lm_intel.log 
+Analysing logs/lm_intel.log
+(INTEL).CCompL: 22
+(INTEL).DbgL: 1
+(INTEL).FCompL: 7
+(INTEL).IB87C5F90: 22
+```
+
+By default it analyses logs/test.log in the current working directory but it supports these flags:
+
++-----+--------------+
+| -h  | Prints flags |
+| -l logfile | Analyse logfile |
+| -d | Enable debug output |
++-----+-------------+
---
+++ maxlic.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+# This tool runs through a FlexLM license server log and prints the max usage
+# of every license type that's checked out from it.
+
+# Owain Kenway, February 2016
+
+# Set the strings we detect as checking a license in/out.
+# Maybe should do this as arguments?
+LIC_IN = 'IN:'
+LIC_OUT = 'OUT:'
+
+# Main function
+if __name__ == '__main__':
+   import argparse
+
+   parser = argparse.ArgumentParser(description='Print max usage.')
+   parser.add_argument('-l', metavar='logfile', type=str, help='Log file.')
+   parser.add_argument('-d', action='store_true', help='Enable debug output.')
+
+   args = parser.parse_args()
+
+# Debug mode.
+   if args.d:
+      print(args)
+
+# Some sensible default log.
+   logfile = 'logs/test.log'
+
+   if (args.l != None):
+      logfile = args.l
+
+   print('Analysing ' + logfile)
+
+# usage and maxusage are dicts that hold the "current" usage and maximum value
+# thereof respectively.
+   usage = {}
+   maxusage = {}
+ 
+   with open(logfile, 'r') as l:
+      for line in l:
+         fields = line.split()
+
+# Drop any lines with not enough fields to check.
+         if len(fields) > 3 :
+# For the purposes of this script, a "product" is the (license mananger
+# ID) dot the product name.  E.g. (INTEL).CCompL
+
+# Detect checking in a license.
+            if (fields[2] == LIC_IN):
+               product = fields[1] + '.' + fields[3].strip('"')
+# Check to see if we've seen this product before and if so subtract 1 from
+# usage.
+               if product in usage:
+                  usage[product] = usage[product] - 1
+               else:
+# Technically if we reach this point the world has gone very wrong.
+# But we could also have a partial license file.
+# In this instance we want to set the usage of the product to zero.
+                  print('WARNING: Checking in license that hasn\'t been checked out. Setting usage for ' + product +  ' to zero.')
+                  usage[product] = 0
+                  maxusage[product] = 0
+
+# We can't have negative usage but with a partial log file we can.  
+# Reset to zero if we get this.
+               if usage[product] < 0:
+                  print('WARNING: Negative usage detected. Setting usage for ' + product +  ' to zero.')
+                  usage[product] = 0
+               if args.d: print('in: ' + fields[1] + '.' + fields[3].strip('"') + ': ' + str(usage[product]))
+
+# Detect checking out a license.
+            elif (fields[2] == LIC_OUT): 
+               product = fields[1] + '.' + fields[3].strip('"')
+
+# If we've seen this product before, add 1 to usage and set maxusage if this
+# is the most we've seen so far.
+               if product in usage:
+                  usage[product] = usage[product] + 1
+                  if maxusage[product] < usage[product]:
+                     maxusage[product] = usage[product]
+                     if args.d: print('New max for ' + product + ': ' + str(usage[product]))
+               else:
+# If we've not seen this product before, create new entries in usage and 
+# maxusage.
+                  usage[product] = 1
+                  maxusage[product] = 1
+               if args.d: print('out: ' + fields[1] + '.' + fields[3].strip('"') + ': ' + str(usage[product]))
+
+
+   l.close()
+
+# Print the usage for all of the products we've discovered.
+   for record in maxusage:
+      print(record + ": " + str(maxusage[record])) 
